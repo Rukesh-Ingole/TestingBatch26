1fedd76fd68640b631b2c19ec330b01f88c824d6
Rukesh-Ingole <67459565+Rukesh-Ingole@users.noreply.github.com>
2020-06-25 23:56:07 -1200

    Update Demo.java

--- Selenium GIT/src/GitHub/Demo.java
+++ Selenium GIT/src/GitHub/Demo.java
@@ -6,5 +6,6 @@ public static void main(String[] args) {
 	System.out.println("Scayrotriposalacospo is long name");
 	System.out.println("Hello RBI");
 	System.out.println("CJC=======Pune");
+	System.out.println("RukeshIngole");
 }	
 }

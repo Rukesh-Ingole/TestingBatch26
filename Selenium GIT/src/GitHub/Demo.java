89372b0b6676259fc111b95d724b6a945ce8b1fc
RUKESH INGOLE <rukeshingole>
2020-06-26 15:00:59 -1200

    New Two

--- Selenium GIT/src/GitHub/Demo.java
+++ Selenium GIT/src/GitHub/Demo.java
@@ -4,6 +4,6 @@ public class Demo
 {
 public static void main(String[] args) {
 	System.out.println("Scayrotriposalacospo is long name");
-	System.out.println("Hello CJC");
+	System.out.println("Hello RB");
 }	
 }

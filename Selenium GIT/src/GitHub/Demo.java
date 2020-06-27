246c02614c54d5f490415e5a98146cf391b3312d
RUKESH INGOLE <rukeshingole>
2020-06-26 14:49:41 -1200

    New change

--- Selenium GIT/src/GitHub/Demo.java
+++ Selenium GIT/src/GitHub/Demo.java
@@ -3,6 +3,6 @@ package GitHub;
 public class Demo 
 {
 public static void main(String[] args) {
-	System.out.println("Scayrotriposalacospo");
+	System.out.println("Scayrotriposalacospo is long name");
 }	
 }

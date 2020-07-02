e1902db69e530bf40f3a778c70f65213ecd8be07
RUKESH INGOLE <rukeshingole>
2020-07-01 16:25:26 -1200

    New Project

---
+++ Selenium Jenkins/src/Jenkin/Demo.java
@@ -0,0 +1,8 @@
+package Jenkin;
+
+public class Demo 
+{
+public static void main(String[] args) {
+	System.out.println("Ralph lauren");
+}
+}
---
+++ Selenium Jenkins/src/Jenkin/Demo1.java
@@ -0,0 +1,7 @@
+package Jenkin;
+
+public class Demo1 {
+public static void main(String[] args) {
+	System.out.println("Armani");
+}
+}
